72cf722275df98c38665913599c8cfc1ca5c52f9
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-08-31 14:28:29 +0200

    huesgrhuiergohierajliöesgröhoiegr

--- TE21B_Gitdemo/Program.cs
+++ TE21B_Gitdemo/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Goodbye");
+
 // git config --global user.name "Förnamn Efternamn"
 // git config --global user.email "fornamn.efternamn@elev.ga.ntig.se"
