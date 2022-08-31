4dd16c4f8a4a71a07a6ee5e93fdb687f28581c9a
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-08-31 14:22:25 +0200

    Initial commit

---
+++ TE21B_Gitdemo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// git config --global user.name "Förnamn Efternamn"
+// git config --global user.email "fornamn.efternamn@elev.ga.ntig.se"
